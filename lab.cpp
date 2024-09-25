ed28ca93016dd529bf1356de54b6c28be5d90bff
EditaGharibyan <editagharibyan.tt219-1@polytechnic.am>
2024-09-25 10:56:40 +0400

    Create lab.cpp
    product1

---
+++ lab.cpp
@@ -0,0 +1,40 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+#include <ctime>
+using namespace std;
+class Program{
+public:
+    void solution(){
+          int p = 1;
+    int index1, index2;
+   
+    
+    for(int i = 0; i < 10; i++){
+        for(int j = i + 1; j < 10; j++){
+            int temp = arr[i] * arr[j];
+            if(temp > p){
+                p = temp;
+                index1 = i;
+                index2 = j;
+            }
+        }
+    }
+    cout << endl << "The largest product is " << arr[index1] << "*" << arr[index2] << " =" <<  p;      
+    }
+    void generateArray(){
+        srand(time(0));
+        for(int i = 0; i < 10 ; i++){
+            arr[i] = rand() % 10;
+            cout << arr[i] << " ";
+        }
+    }
+private:
+int arr[10];
+};
+int main() {
+
+  Program prog;
+  prog.generateArray();
+  prog.solution();
+    
+}
