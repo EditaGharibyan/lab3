854a62c5b1db011276e189822111789b54ded37b
EditaGharibyan <editagharibyan.tt219-1@polytechnic.am>
2024-10-22 21:07:26 +0400

    Update lab.cpp

--- lab.cpp
+++ lab.cpp
@@ -11,30 +11,42 @@ public:
     
     for(int i = 0; i < 10; i++){
         for(int j = i + 1; j < 10; j++){
+            if(arr[i]<0 && arr[j<0]){
+                int temp = abs(arr[i] * arr[j]);
+            if(temp > p){
+                p = temp;
+                index1 = i;
+                index2 = j;
+            }
+            }
+            
+            else{
             int temp = arr[i] * arr[j];
             if(temp > p){
                 p = temp;
                 index1 = i;
                 index2 = j;
             }
+            
+            }
         }
     }
     cout << endl << "The largest product is " << arr[index1] << "*" << arr[index2] << " =" <<  p;      
     }
-    void generateArray(){
-        srand(time(0));
-        for(int i = 0; i < 10 ; i++){
-            arr[i] = rand() % 10;
-            cout << arr[i] << " ";
-        }
-    }
+  //  void generateArray(){
+//        srand(time(0));
+  //      for(int i = 0; i < 10 ; i++){
+//            arr[i] = rand() % 10;
+ //           cout << arr[i] << " ";
+ //      }
+ //   }
 private:
-int arr[10];
+int arr[10]={-100,-20,10,2,3,4};
 };
 int main() {
 
   Program prog;
-  prog.generateArray();
+  //prog.generateArray();
   prog.solution();
     
 }
